f3c602c180a28d367b1c0c6b978ec2b30c21e3a2
Brayahn <brayahnmainah@gmail.com>
2021-10-04 10:27:05 +0300

    User Model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, , :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :timeoutable,
+         :recoverable, :rememberable, :validatable
+
+         def timeout_in
+          return 1.year if admin?
+          1.days
+         end
+end
